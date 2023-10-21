aef6fe3b99450c5a9ce144cd288bf9f5dc0d6974
sagarsharma21 <007sharma.sagar@gmail.com>
2023-10-21 16:59:28 +0530

    Add lifecycle hooks

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@
  */
 
 import React from 'react';
- 
+ import { useState, useEffect } from 'react';
+
 import {
+  ActivityIndicator,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -25,12 +27,36 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-
+import {setupPlayer, addTrack} from "../musicPlayerServices";
 
 
 
 function App(): JSX.Element {
   
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+  async function setup() {
+    let isSetup = await setupPlayer();
+
+    if (isSetup) {
+      await addTrack()
+    }
+
+    setIsPlayerReady(isSetup)
+  }
+  
+  useEffect(() => {
+   setup()
+  }, [])
+  
+  if (!isPlayerReady) {
+    return(
+      <SafeAreaView>
+        <ActivityIndicator/>
+      </SafeAreaView>
+    )
+  }
+
   return (
 
     <ScrollView>
@@ -41,22 +67,27 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  
+  // sectionContainer: {
+  //   marginTop: 32,
+  //   paddingHorizontal: 24,
+  // },
+  // sectionTitle: {
+  //   fontSize: 24,
+  //   fontWeight: '600',
+  // },
+  // sectionDescription: {
+  //   marginTop: 8,
+  //   fontSize: 18,
+  //   fontWeight: '400',
+  // },
+  // highlight: {
+  //   fontWeight: '700',
+  // },
+  container: {
+    flex:1,
+  }
+
 });
 
 export default App;
