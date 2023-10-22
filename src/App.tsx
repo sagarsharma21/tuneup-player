94c4af071598349918594d3b4b61f2a5af9e7287
sagarsharma21 <007sharma.sagar@gmail.com>
2023-10-22 08:53:52 +0530

    Minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ function App(): JSX.Element {
   return (
 
     <ScrollView>
-        <View>Fine</View>
+        <View><Text>Fine</Text></View>
     </ScrollView>
     
   );
--- src/constants.ts
+++ src/constants.ts
@@ -4,17 +4,26 @@ export const playListData: Track[] = [
     {
         id:1,
         url: require('./assets/indian-trap.mp3'),
+        artwork:'https://pixabay.com/vectors/vinyl-platinum-disk-music-dj-2241789/'
     },
     {
         id:2,
-        url: require('./assets/kabhi-pyar-kiya.mp3')
+        url: require('./assets/kabhi-pyar-kiya.mp3'),
+        artwork:'https://pixabay.com/photos/music-feel-feeling-violin-feelings-4683335/'
     },
     {
         id:3,
-        url: require('./assets/lovely-days.mp3')
+        url: require('./assets/lovely-days.mp3'),
+        artwork:'https://pixabay.com/photos/people-woman-guitar-epiphone-2572167/'
     },
     {
         id:4,
-        url: require('./assets/trap-hip-hop.mp3')
+        url: require('./assets/trap-hip-hop.mp3'),
+        artwork:'https://www.shutterstock.com/image-vector/trap-music-original-logo-design-typography-1438910417'
+    },
+    {
+        id:5,
+        url: require('./assets/inside-you.mp3'),
+        artwork:'https://pixabay.com/photos/leva-the-cradle-of-your-soul-8148223/'
     }
 ];
